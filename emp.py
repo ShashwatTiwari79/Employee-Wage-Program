8e6b0dd73646f3da6a3d9c678e21393e57a9a83e
SHASHWATTIWARI07 <shashwattiwari0709@gmail.com>
2025-02-21 00:44:18 +0530

    Python script to check the attendance of an employee

--- emp.py
+++ emp.py
@@ -1,2 +1,13 @@
-print("Welcome to Employee wage computation")
+import random
+
+def check_attendance():
+    print("Welcome to employee wages computation program")
+    attendance = random.choice([1,0])
+    if attendance == 1:
+        print("The employee is present")
+    else:
+        print("The employee is absent")
+if __name__ == "__main__":
+    check_attendance()
+
 
