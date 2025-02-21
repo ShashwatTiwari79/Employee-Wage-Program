dd6cad43a67939fa15821fc589eccf58871d5d87
SHASHWATTIWARI07 <shashwattiwari0709@gmail.com>
2025-02-21 10:49:34 +0530

    This python script is used to check whether the employee was present for the whole day or half day

--- emp.py
+++ emp.py
@@ -2,18 +2,23 @@ import random
 
 def check_attendance():
     print("Welcome to employee wages computation program")
-    attendance = random.choice([1,0])
-    if attendance == 1:
-        print("The employee is present")
+    attendance = random.choice([0,1,2])
+    if attendance == 2:
+        print("The employee is present for the full day")
+    elif attendance == 1:
+        print("The employee is present for the half day")
     else:
         print("The employee is absent")
     return attendance
 def emp_daily_wage():
     wage_per_hour = 20
     emp_check = check_attendance()
-    if emp_check == 1:
+    if emp_check == 2:
         daily_wage = wage_per_hour*8
-        print(f"The employee was present for the entire day so daily wage is : {daily_wage}")
+        print(f"The employee is present for the entire day so daily wage is : {daily_wage}")
+    if emp_check == 1:
+        daily_wage = wage_per_hour*4
+        print("The employee is present for the half day")
     else:
         daily_wage = 0
         print(f"The employee was absent for the entire day so daily wage is : {daily_wage}")
